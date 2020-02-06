8d99d7408167e8a211c3a0815776e6cef756a842
Likholetov Dmitry <likholetov1990@gmail.com>
2020-02-06 11:04:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let startButton = document.getElementById('start');
+console.log('startButton: ', startButton);
+
+let plusButton = document.getElementsByTagName('button');
+console.log('plusButton: ', plusButton);
+
+let checkDeposit = document.querySelector('#deposit-check');
+console.log('checkDeposit: ', checkDeposit);
+
+let additionalIncome = document.querySelectorAll('.additional_income-item');
+console.log('additionalIncome: ', additionalIncome);
+
+
